0c224ea9683554770a043c0ab65a9da34d40ce49
Daniel isiogugu <dannydix555@gmail.com>
2022-02-06 13:23:39 +0100

    working update

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,13 +1,33 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
 
 class Loading extends StatefulWidget {
-  const Loading({Key? key}) : super(key: key);
+  // const Loading({Key? key}) : super(key: key);
 
   @override
   _LoadingState createState() => _LoadingState();
 }
 
 class _LoadingState extends State<Loading> {
+
+
+
+  void getTime() async {
+    var url = Uri.parse("http://worldtimeapi.org/api/timezone/Africa/Lagos");
+    var response = await get(url);
+
+    print(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
