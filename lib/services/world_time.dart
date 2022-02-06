b842c27afc26f6f87b7b1da0fe6545184f292ebf
Daniel isiogugu <dannydix555@gmail.com>
2022-02-06 16:46:48 +0100

    worked on loading page UI

---
+++ lib/services/world_time.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:intl/intl.dart';
+
+class WorldTime{
+
+  String? location; //location name for UI
+  String? time; //time for location
+  String? flag; //url to an asset for flag
+  String? rrl; //location url for api
+
+  WorldTime({this.location, this.flag, this.rrl});
+
+
+  Future<void> getTime() async {
+
+    try{
+      var url = Uri.parse('http://worldtimeapi.org/api/timezone/$rrl');
+      var response = await get(url);
+
+      Map data = jsonDecode(response.body);
+
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1,3);
+
+      // print(datetime);
+      // print(offset);
+
+      //  create date time object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+
+      time = DateFormat.jm().format(now);
+    }catch (e) {
+      print('caught error $e');
+      time = 'could not get time data';
+    }
+
+  }
+}
